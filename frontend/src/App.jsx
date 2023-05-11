c59d73eeaae67ac44b2064972f47998bdfc15fbe
etienne chamarier <etienne.chamarier@gmail.com>
2023-05-11 16:03:17 +0200

    ajout api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import Home from "./pages/Home";
-
+import test from "./api/chatgpt_api";
 import "./App.css";
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [data, setData] = useState(test.content);
   return (
     <div className="App">
       <Home />
-      <p>coucou</p>
+      <p>{data}</p>
     </div>
   );
 }
--- frontend/src/api/chatgpt_api.js
+++ frontend/src/api/chatgpt_api.js
@@ -1,11 +1,18 @@
-const { Configuration, OpenAIApi } = require("openai")
+import { Configuration, OpenAIApi } from "openai"
 
 const configuration = new Configuration({
-//   apiKey: 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 // Idéalement, vous aurez mis votre clé api dans l'env
+//   apiKey: "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",
 })
 const openai = new OpenAIApi(configuration)
 
 const completion = await openai.createChatCompletion({
   model: "gpt-3.5-turbo",
-  messages: [{ role: "user", content: "Peux-tu me proposer 2 detinations, [europe / hors-Europe] durant la période [estival / hyvernal], plutôt [ville / nature] proche de la [montange / mer], je souhaiterai profiter d'activité [sportives / culturel / culinaire / reposantes]. Je ne veux pas d'introduction n'y de conclusion, le tout écris en maximum de trois lignes" }],
+  messages: [{ role: "user", content: "Peux-tu me proposer 2 destinations, [europe / hors-Europe] durant la période [estival / hyvernal], plutôt [ville / nature] proche de la [montange / mer], je souhaiterai profiter d'activité [sportives / culturel / culinaire / reposantes]. Je ne veux pas d'introduction n'y de conclusion, le tout écris en maximum de trois lignes" }],
 })
+
+console.log(completion.data.choices[0].message)
+
+const test = completion.data.choices[0].message
+
+
+export default test;
