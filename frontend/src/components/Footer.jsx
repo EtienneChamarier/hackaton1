13ce59247b78b3de98fd005ea41082b6c5b22d78
JeanMMillet <millet.jm@live.com>
2023-05-11 21:42:09 +0200

    fix bug footer and navbar

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,10 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+
+import { useNavigate } from "react-router-dom";
+
 function Footer() {
+  const navigate = useNavigate();
   return (
     <footer>
-      <p>A PROPOS</p>
-      <p>OFFRES D'EMPLOIE</p>
-      <p>CONTACT</p>
-      <p>MENTIONS LEGALES</p>
+      <li onClick={() => navigate("/about")}>A PROPOS</li>
+      <p onClick={() => navigate("/contact")}>OFFRES D'EMPLOIS</p>
+      <p onClick={() => navigate("/contact")}>CONTACT</p>
+      <p onClick={() => navigate("/mentions")}>MENTIONS LEGALES</p>
       <span>© 2023 Tripotevisor LLC Tous droits réservés.</span>
     </footer>
   );
