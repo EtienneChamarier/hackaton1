441bdb58713b2eb80505bcdbade879af2b70c001
Thomas-Fch <thomas.fachinetti@gmail.com>
2023-05-11 21:00:28 +0200

    ajout des pages, Home, About, Mentions legales terminées | footer ajouté | menu burger ajouté

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,10 @@
-import QuestionPage from "./TestPage";
-import questions from "./assets/data";
 import "./App.scss";
+import Mentions from "./pages/Mentions";
 
 function App() {
-  const promptAnswers = [];
-  function handleGenerateAnswer() {
-    console.info(
-      `Peux-tu me proposer 2 destinations, ${promptAnswers[0]} durant la période ${promptAnswers[1]}, plutôt ${promptAnswers[2]} proche de la ${promptAnswers[3]}, je souhaiterai profiter d'activité ${promptAnswers[4]}. Je ne veux pas d'introduction n'y de conclusion, le tout écris en maximum de trois lignes`
-    );
-  }
-
   return (
     <div className="App">
-      {questions.map((question) => {
-        return (
-          <QuestionPage promptAnswers={promptAnswers} answers={question} />
-        );
-      })}
-      {/* <QuestionPage promptAnswers={promptAnswers} answers={answers} />
-      <QuestionPage promptAnswers={promptAnswers} answers={answers2} /> */}
-      <button type="button" onClick={handleGenerateAnswer}>
-        TEST
-      </button>
+      <Mentions />
     </div>
   );
 }
