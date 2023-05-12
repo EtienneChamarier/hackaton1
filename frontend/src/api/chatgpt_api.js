8b2252279125ed88f772e723add2dd15ff6ee097
etienne chamarier <etienne.chamarier@gmail.com>
2023-05-12 09:20:49 +0200

    add contact

--- frontend/src/api/chatgpt_api.js
+++ frontend/src/api/chatgpt_api.js
@@ -1,17 +1,17 @@
-import { Configuration, OpenAIApi } from "openai"
+// import { Configuration, OpenAIApi } from "openai"
 
-const configuration = new Configuration({
-  apiKey: "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",
-});
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({
+//   apiKey: "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",
+// });
+// const openai = new OpenAIApi(configuration);
 
-const completion = await openai.createChatCompletion({
-  model: "gpt-3.5-turbo",
-  messages: [{ role: "user", content: "Peux-tu me proposer 2 destinations, [europe / hors-Europe] durant la période [estival / hyvernal], plutôt [ville / nature] proche de la [montange / mer], je souhaiterai profiter d'activité [sportives / culturel / culinaire / reposantes]. Je ne veux pas d'introduction n'y de conclusion, le tout écris en maximum de trois lignes" }],
-});
+// const completion = await openai.createChatCompletion({
+//   model: "gpt-3.5-turbo",
+//   messages: [{ role: "user", content: "Peux-tu me proposer 2 destinations, [europe / hors-Europe] durant la période [estival / hyvernal], plutôt [ville / nature] proche de la [montange / mer], je souhaiterai profiter d'activité [sportives / culturel / culinaire / reposantes]. Je ne veux pas d'introduction n'y de conclusion, le tout écris en maximum de trois lignes" }],
+// });
 
-console.info(completion.data.choices[0].message);
+// console.info(completion.data.choices[0].message);
 
-const test = completion.data.choices[0].message;
+// const test = completion.data.choices[0].message;
 
-export default test;
+// export default test;
